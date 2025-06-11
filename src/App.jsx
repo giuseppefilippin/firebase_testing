e3bdb067d6304bcc35116f4b637a5720463dfd64
Giuseppe Filippin <giuseppe.ferreira@pucpr.edu.br>
2025-06-11 14:43:38 -0300

    sa

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,72 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { Auth } from "./components/auth";
 import { db } from "./config/firebase";
-import { getDocs, collection } from "firebase/firestore";
+import { getDocs, collection, addDoc } from "firebase/firestore";
 
 function App() {
   const [comentList, setComentList] = useState([]);
+
+  const [newComent, setNewComent] = useState("");
+
   const comentsCollectionRef = collection(db, "comentarios");
+
+  const getComentList = async () => {
+      try {
+        const data = await getDocs(comentsCollectionRef);
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setComentList(filteredData);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    
+    const onSubmitComent = async () => {
+    try {
+      await addDoc(comentsCollectionRef, { coments: newComent });
+      getComentList();
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   useEffect(() => {
     const getComentList = async () => {
       try {
         const data = await getDocs(comentsCollectionRef);
-        const filteredData = data.docs.map((doc) => ({...doc.data(), id: doc.id}));
-        console.log(filteredData);
-        setComentList(filteredData); 
-      } catch(err) {console.error(err)};
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setComentList(filteredData);
+      } catch (err) {
+        console.error(err);
+      }
     };
     getComentList();
-  }, []);
-  
+  }, [onSubmitComent]);
+
+
 
   return (
     <div className="App">
       <Auth />
-    <div>
 
-    {comentList.map((comentarios) => (
       <div>
-        <h1>{comentarios.coments}</h1>
-        </div>
-      ))}
+        <input
+          placeholder="comentarios"
+          onChange={(e) => setNewComent(e.target.value)}
+        />
+        <button onClick={onSubmitComent}>Enviar</button>
+      </div>
+      <div>
+        {comentList.map((comentarios) => (
+          <div>
+            <h1>{comentarios.coments}</h1>
+          </div>
+        ))}
       </div>
     </div>
   );
