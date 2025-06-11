584482cedb0ba0c1eeead55df437e3ecccbb3631
Giuseppe Filippin <giuseppe.ferreira@pucpr.edu.br>
2025-06-11 14:49:39 -0300

    sa

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { Auth } from "./components/auth";
 import { db } from "./config/firebase";
-import { getDocs, collection, addDoc } from "firebase/firestore";
+import {
+  getDocs,
+  collection,
+  addDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 
 function App() {
   const [comentList, setComentList] = useState([]);
@@ -12,25 +18,30 @@ function App() {
   const comentsCollectionRef = collection(db, "comentarios");
 
   const getComentList = async () => {
-      try {
-        const data = await getDocs(comentsCollectionRef);
-        const filteredData = data.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-        setComentList(filteredData);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    
-    const onSubmitComent = async () => {
+    try {
+      const data = await getDocs(comentsCollectionRef);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setComentList(filteredData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const onSubmitComent = async () => {
     try {
       await addDoc(comentsCollectionRef, { coments: newComent });
       getComentList();
     } catch (err) {
       console.error(err);
     }
+  };
+
+  const deleteComent = async (id) => {
+    const comentDoc = doc(db, "comentarios", id);
+    await deleteDoc(comentDoc);
   };
 
   useEffect(() => {
@@ -49,8 +60,6 @@ function App() {
     getComentList();
   }, [onSubmitComent]);
 
-
-
   return (
     <div className="App">
       <Auth />
@@ -66,6 +75,7 @@ function App() {
         {comentList.map((comentarios) => (
           <div>
             <h1>{comentarios.coments}</h1>
+            <button onClick={() => deleteComent(comentarios.id)}>Delete</button>
           </div>
         ))}
       </div>
